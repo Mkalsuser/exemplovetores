ff6c1ee66618127b019c87c11ed4b6083c622a19
Mkalsuser <nikitifabandi@gmail.com>
2025-11-17 18:50:41 -0300

    marrocos

---
+++ exemplos/Exemplo12/Exemplo12/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Security;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            const int QTD_CDL = 3;
+            const int QTD_LIN = 3;
+            int[,] matriz = new int[QTD_LIN, QTD_CDL];
+
+            matriz = cadastrarNumeros(QTD_LIN, QTD_CDL);
+
+            Console.WriteLine("\n -------MEMORIZE-------- \n");
+
+            exibirDados(matriz);
+            Thread.Sleep(5000);
+            Console.Clear();
+            int sorteioLinha = random.Next(0, QTD_LIN);
+            int sorteioColuna = random.Next(0, QTD_CDL);
+
+            Console.Write("\nQual número consta na posição: linha {0}, coluna {1} da matriz: ", (sorteioLinha+1), (sorteioColuna+1));
+            int num = int.Parse(Console.ReadLine());
+
+            if (verifica(matriz[sorteioLinha, sorteioColuna], num) == true)
+                Console.WriteLine("\nParabéns! Você acertou!\n");
+            else
+                Console.WriteLine("\nQue pena! Você errou! Número correto seria: {0}\n", matriz[sorteioLinha, sorteioColuna]);
+            exibirDados(matriz);
+        }
+        static int[,] cadastrarNumeros(int nLin, int nCol)
+        {
+            int[,] n = new int[nLin, nCol];
+            Random random = new Random();
+
+            for (int i = 0; i < nLin; i++)
+                for (int c = 0; c < nCol; c++)
+                    n[i, c] = random.Next(0, 50);
+            return n;
+        }
+        static Boolean verifica(int numeroMatriz, int numUsuario)
+        {
+            return numeroMatriz == numUsuario ? true : false;
+        }
+        static void exibirDados(int[,] n)
+        {
+            for (int i = 0; i < n.GetLength(0); i++)
+            {
+                for (int j = 0; j < n.GetLength(1); j++)
+                {
+                    Console.Write("{0:D2}\t", n[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ exemplos/exemplo 11/exemplo 11/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int QTD_COL = 3;
+            const int QTD_LIN = 5;
+            double[,] notas = new double[QTD_LIN, QTD_COL];
+
+            notas = cadastrarNotas (QTD_LIN, QTD_COL);
+
+            Console.WriteLine("\n *** Notas cadastradas na Matriz *** \n");
+            exibirDados(notas);
+            Console.ReadKey();
+        }
+        static double[,] cadastrarNotas(int nLin, int nCol)
+        {
+            double[,] n = new double[nLin, nCol]; 
+            for (int i = 0; 1 < nLin; i++)
+            {
+                Console.WriteLine("\n Notas do {0}° aluno", 1 + 1);
+                for (int c = 0; c < nCol; c++)
+                {
+                    Console.WriteLine(" {0}° nota: ", c + 1);
+                    n[1, c] = double.Parse(Console.ReadLine());
+
+                }
+            }
+            return n;
+        }
+    
+        static void exibirDados(double[,] nt)
+        {
+            Console.WriteLine("\n Aluno \t \tNotal1 \t \tNota2 \t\tNota3 \n");
+            for (int i = 0; i < nt.GetLength(0); i++)
+            {
+                Console.WriteLine("{0}° Aluno: ", i + 1);
+                for (int j = 0; j < nt.GetLength(1); j++)
+                {
+                    Console.WriteLine("\t {0:N1}\t", nt[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
